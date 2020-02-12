1e4c1d6f95b37daa6d8e8fb9f4b954fbb885a807
Martin Engelsøy Norum <martin.norum@gmail.com>
2020-02-12 20:51:43 +0100

    Remove theme from base gatsby files

--- webapp/gatsby-ssr.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { ThemeProvider } from "theme-ui";
-import theme from 'gatsby-plugin-theme-ui'
-export const wrapRootElement = ({ element }) => (
-    <ThemeProvider>
-        {element}
-    </ThemeProvider>
-)
--- webapp/src/gatsby-plugin-theme-ui/index.js
+++ webapp/src/gatsby-plugin-theme-ui/index.js
@@ -1,9 +1,28 @@
-import {future} from '@theme-ui/presets'
-
 export default {
-    ...future,
-   
-    button: {
-        'fontWeight': 700
+    colors: {
+      text: '#000',
+      background: '#fff',
+      primary: '#07c',
+    },
+    fonts: {
+      body: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+      heading: 'Georgia, serif',
+    },
+    fontWeights: {
+      body: 400,
+      heading: 700,
+    },
+    styles: {
+      h1: {
+        fontSize: 32,
+        fontFamily: 'heading',
+        fontWeight: 'heading',
+        color: 'primary',
+        mt: 4,
+        mb: 2,
+      },
+      h2: {
+          color: 'pink'
+      }
     }
-}
+  }
