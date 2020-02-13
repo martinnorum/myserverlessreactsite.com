056a77911bd9cd89117bcabbd4088ebb0e559049
Martin Engelsøy Norum <martin.norum@gmail.com>
2020-02-13 17:28:55 +0100

    Refactored index

--- webapp/src/pages/index.js
+++ webapp/src/pages/index.js
@@ -6,6 +6,16 @@ import SEO from "../components/seo"
 import {useAuth} from 'react-use-auth'
 import { Heading, Button } from 'rebass'
 
+const LoggedIn = ({name, action}) => (<>
+  <p>hello {name}</p>
+  <Button bg='highlight' onClick={action}>Log out</Button>
+</>)
+
+const LoggedOut = ({action}) => (<>
+  <p>Not logged in yet</p>
+  <Button bg='highlight' onClick={action}>Get Started</Button>
+</>)
+
 const IndexPage = () =>  {
   const { isAuthenticated, user, login, logout } = useAuth()
   return (
@@ -13,9 +23,10 @@ const IndexPage = () =>  {
     <SEO title="Markdown Landing Page" />
     <Heading fontSize={[5, 6, 7]}>Markdown Landing Page</Heading>
     <p>Write a landing page for anything</p>
-    {isAuthenticated() ? <p>hello {user.name}</p> : <p>Not logged in yet</p>}
-    <Button bg='highlight' onClick={login}>Get Started</Button>
-    <Button bg='highlight' onClick={logout}>Log out</Button>
+    { isAuthenticated()
+      ? <LoggedIn name={user.name} action={logout}></LoggedIn>
+      : <LoggedOut action={login}></LoggedOut>
+    }
   </Layout>
 )}
 
